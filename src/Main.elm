89f19d524913ac74a1142a2fea3b3dfacc1d0562
David Harting <davideharting@gmail.com>
2019-12-06 15:37:10 -0500

    Basic form with toggle-visible password

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, h2, input, p, text)
-import Html.Attributes exposing (class, placeholder, value)
+import Html exposing (Html, button, div, form, h1, h2, input, label, p, text)
+import Html.Attributes exposing (class, for, name, placeholder, type_, value)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -16,15 +16,32 @@ main =
 
 
 -- model
+-- Control show / hide password
+
+
+type alias SignUpForm =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , showPassword : Bool
+    }
 
 
 type alias Model =
-    { content : String, count : Int }
+    { content : String, count : Int, form : SignUpForm }
 
 
 init : Model
 init =
-    { content = "", count = 0 }
+    { content = ""
+    , count = 0
+    , form =
+        { name = ""
+        , password = ""
+        , passwordAgain = ""
+        , showPassword = False
+        }
+    }
 
 
 
@@ -35,6 +52,14 @@ type Msg
     = Increment
     | Decrement
     | Change String
+    | FormTogglePasswordVisiblity
+    | FormName String
+    | FormPassword String
+
+
+
+-- | FormPassword String
+-- | FormPasswordAgain String
 
 
 update : Msg -> Model -> Model
@@ -48,6 +73,36 @@ update msg model =
 
         Change newContent ->
             { model | content = newContent }
+
+        FormTogglePasswordVisiblity ->
+            let
+                oldForm =
+                    .form model
+
+                newForm =
+                    { oldForm | showPassword = Basics.not model.form.showPassword }
+            in
+            { model | form = newForm }
+
+        FormName newName ->
+            let
+                oldForm =
+                    .form model
+
+                newForm =
+                    { oldForm | name = newName }
+            in
+            { model | form = newForm }
+
+        FormPassword newPassword ->
+            let
+                oldForm =
+                    .form model
+
+                newForm =
+                    { oldForm | password = newPassword }
+            in
+            { model | form = newForm }
 
 
 
@@ -75,5 +130,41 @@ view model =
         , div []
             [ input [ class "form-control", placeholder "Text to reverse", value <| .content model, onInput Change ] []
             , p [] [ text <| String.reverse <| .content model ]
+            ]
+        , h2 [] [ text "Forms" ]
+        , form []
+            [ div [ class "form-group" ]
+                [ label [ for "name" ] [ text "Name" ]
+                , input [ class "form-control", name "name", placeholder "Your name", value model.form.name, onInput FormName ] []
+                ]
+            , div [ class "form-group" ]
+                [ label [ for "password" ] [ text "Password" ]
+                , div [ class "input-group" ]
+                    [ input
+                        [ class "form-control"
+                        , name "password"
+                        , placeholder "Ssssshhh"
+                        , type_ <|
+                            if model.form.showPassword == True then
+                                "text"
+
+                            else
+                                "password"
+                        , value model.form.password
+                        , onInput FormPassword
+                        ]
+                        []
+                    , div [ class "input-group-append" ]
+                        [ div [ class "btn btn-info", onClick FormTogglePasswordVisiblity ]
+                            [ text <|
+                                if model.form.showPassword then
+                                    "Hide"
+
+                                else
+                                    "Show"
+                            ]
+                        ]
+                    ]
+                ]
             ]
         ]
